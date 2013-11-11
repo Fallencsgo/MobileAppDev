aeb83cb249b4d4bf62b9b740785f31385ac909ed
Alexander Grant <agrant@java.nas.local>
2013-11-11 10:42:58 -0500

    Added exercise.java

---
+++ exercise.java
@@ -0,0 +1,15 @@
+class Ex1 {
+
+  public static void main(String[] args) {
+   int n;
+   int m;
+   isDivisible(111,3);
+ }
+  public static void isDivisible(int n, int m){
+    if  (n%m == 0){
+     System.out.println("The equation is True");
+   }else{
+     System.out.println("The equation is False");
+}
+}
+}
